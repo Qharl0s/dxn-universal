2fc16d2fad933e6f342022f974fe3148257a05e5
qharl0s <qharl0s@gmail.com>
2023-09-09 21:29:37 -0500

    setting static_root

--- dxn_universal/settings.py
+++ dxn_universal/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATIC_ROOT = BASE_DIR / "static_build" / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', 'media')
