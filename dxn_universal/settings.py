0b8994e6c9399ecf299e8cecf09851d18589269a
qharl0s <qharl0s@gmail.com>
2023-09-10 22:04:14 -0500

    config4

--- dxn_universal/settings.py
+++ dxn_universal/settings.py
@@ -127,12 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-#STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', 'media')
