b7ff30f5545367d3e24e51fb0ac9e5a35eae283e
qharl0s <qharl0s@gmail.com>
2023-09-10 22:04:49 -0500

    config url

--- dxn_universal/urls.py
+++ dxn_universal/urls.py
@@ -34,5 +34,8 @@ urlpatterns = [
     path('contactanos', contactanos, name='contactanos'),
     path('registrate', planes, name='planes'),
     path('summernote/', include('django_summernote.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
