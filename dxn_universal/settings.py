853deb7a532a02f7ecb8696cad4407e11481a28a
qharl0s <qharl0s@gmail.com>
2023-09-10 21:32:44 -0500

    staticfiles

--- dxn_universal/settings.py
+++ dxn_universal/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 #STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
