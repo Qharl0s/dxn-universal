ed1540025fcf41ec8b7fc1f038bf041b0afefa24
alarico <alarico@cajalosandes.pe>
2023-09-11 19:43:35 -0500

    configuraciones

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,3 @@
-#!bin/bash
-
-echo "Creando el projecto..."
-python3.9 -m pip install -r requirements.txt
-
-echo "Iniciando migraciones.."
-
-python3.9 .\manage.py makemigrations --noinput
-python3.9 .\manage.py migrate --noinput
-
-echo "Colecciones estaticas.."
-python3.9 .\manage.py collectstatic --noinput --clear
-echo "fin.."
+pip install -r requirements.txt
+python3.9 manage.py collectstatic --no-input
+python3.9 manage.py migrate
